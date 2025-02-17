5997208c1704b7bf38faa7a8aa26eb2e41cf6542
DS3a <33351122+DS3a@users.noreply.github.com>
2025-02-17 20:27:04 +0100

    add accel controller

--- src/local_planner/trajectory_follower.py
+++ src/local_planner/trajectory_follower.py
@@ -1,6 +1,11 @@
 class TrajectoryFollower:
-    def __init__(self, trajectory):
+    def __init__(self, trajectory, ik_controller, robot):
         """
             The trajectory is merely a set of points
         """
         self.trajectory = trajectory
+        self.ik_controller = ik_controller
+        self.robot = robot
+
+    def step(self, t=1/240):
+        pass
